df228c5f9e8ce78dd92507b9ad0106f28ef25a0a
HelloNoa <ohyoungwoo824@gmail.com>
2026-02-13 19:23:21 +0900

    fix

--- src/app/minigame/page.tsx
+++ src/app/minigame/page.tsx
@@ -1,19 +1,10 @@
 import styles from './page.module.css';
+import NoSignal from '@/components/layout/no_signal/no_signal';
 
-export default function Contact() {
+export default function Works() {
   return (
     <div className={styles.page}>
-      <img
-        src="/detail/detail_1.png"
-        alt="main detail image"
-        loading="lazy"
-      />
-      <hr/>
-      <img
-        src="/detail/detail_2.png"
-        alt="main detail image"
-        loading="lazy"
-      />
+      <NoSignal/>
     </div>
   );
 }
--- src/app/works/page.tsx
+++ src/app/works/page.tsx
@@ -1,10 +1,19 @@
 import styles from './page.module.css';
-import NoSignal from '@/components/layout/no_signal/no_signal';
 
-export default function Works() {
+export default function Contact() {
   return (
     <div className={styles.page}>
-      <NoSignal/>
+      <img
+        src="/detail/detail_1.png"
+        alt="main detail image"
+        loading="lazy"
+      />
+      <hr/>
+      <img
+        src="/detail/detail_2.png"
+        alt="main detail image"
+        loading="lazy"
+      />
     </div>
   );
 }
