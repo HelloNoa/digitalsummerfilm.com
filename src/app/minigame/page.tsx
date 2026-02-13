7ade705d2db1cb7567e6475426b1326740ac0cd1
HelloNoa <ohyoungwoo824@gmail.com>
2026-02-13 18:55:35 +0900

    fix

--- src/app/minigame/page.tsx
+++ src/app/minigame/page.tsx
@@ -6,16 +6,12 @@ export default function Contact() {
       <img
         src="/detail/detail_1.png"
         alt="main detail image"
-        width={444}
-        height={250}
         loading="lazy"
       />
       <hr/>
       <img
         src="/detail/detail_2.png"
         alt="main detail image"
-        width={445}
-        height={334}
         loading="lazy"
       />
     </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,16 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import Image from 'next/image';
+import styles from './page.module.css';
 
 export default function Home() {
   return (
     <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          src="/main/detail_1.png"
-          alt="main detail image"
-          width={616}
-          height={616}
-          priority
-        />
-        {/*<div className={styles.intro}>*/}
-        {/*</div>*/}
-      </main>
+      <img
+        src="/main/detail_1.png"
+        alt="main detail image"
+        loading="lazy"
+      />
+      {/*<div className={styles.intro}>*/}
+      {/*</div>*/}
     </div>
   );
 }
